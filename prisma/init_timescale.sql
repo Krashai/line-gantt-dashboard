64f572a0bcdd847802f768e3477676d3a022bc8a
Lukasz Bialecki <bialy106@gmail.com>
2026-03-12 22:46:09 +0100

    refactor: Portable database schema with composite keys and retention SQL

--- prisma/init_timescale.sql
+++ prisma/init_timescale.sql
@@ -1,10 +1,11 @@
--- Upewnij się, że rozszerzenie jest aktywne
-CREATE EXTENSION IF NOT EXISTS timescaledb;
+-- 1. Upewnij się, że rozszerzenie jest aktywne
+CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;
 
--- Konwersja tabel na Hypertables
-SELECT create_hypertable('machine_status_history', 'time', if_not_exists => true);
-SELECT create_hypertable('scrap_events', 'time', if_not_exists => true);
+-- 2. Konwersja tabel na Hypertables (wymaga klucza kompozytowego z kolumną 'time')
+-- Uwaga: migrate_data => true pozwala na konwersję niepustych tabel
+SELECT create_hypertable('machine_status_history', 'time', if_not_exists => true, migrate_data => true);
+SELECT create_hypertable('scrap_events', 'time', if_not_exists => true, migrate_data => true);
 
--- Retention policy
-SELECT add_retention_policy('machine_status_history', INTERVAL '90 days', if_not_exists => true);
-SELECT add_retention_policy('scrap_events', INTERVAL '90 days', if_not_exists => true);
+-- 3. Ustawienie polityki retencji (30 dni)
+SELECT add_retention_policy('machine_status_history', INTERVAL '30 days', if_not_exists => true);
+SELECT add_retention_policy('scrap_events', INTERVAL '30 days', if_not_exists => true);
